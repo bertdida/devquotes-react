e6eff203db3cd921b802de00c4840e06ff5f3114
bertdida <38489033+bertdida@users.noreply.github.com>
2019-12-01 16:33:51 +0800

    Removed unused import

--- src/components/Quote/AdminActions.js
+++ src/components/Quote/AdminActions.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import CardActions from "@material-ui/core/CardActions";
 import Fab from "@material-ui/core/Fab";
