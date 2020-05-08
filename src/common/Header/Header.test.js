ac7393e29db6f7ea8d2b22037d7d074cfb9020b1
bertdida <38489033+bertdida@users.noreply.github.com>
2020-05-08 21:08:54 +0800

    Fix fo Header tests - added css-mediaquery as dev dependency - implementation of matchMedia in test

--- src/common/Header/Header.test.js
+++ src/common/Header/Header.test.js
@@ -2,12 +2,25 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import mediaQuery from 'css-mediaquery';
 
 import { AuthContext } from 'common/Auth';
 import { ThemeContext } from 'common/Theme';
 import Header from './Header';
 
 afterEach(cleanup);
+
+function createMatchMedia(width) {
+  return query => ({
+    matches: mediaQuery.match(query, { width }),
+    addListener: () => {},
+    removeListener: () => {},
+  });
+}
+
+beforeAll(() => {
+  window.matchMedia = createMatchMedia(window.innerWidth);
+});
 
 function renderHeader(user) {
   return render(
