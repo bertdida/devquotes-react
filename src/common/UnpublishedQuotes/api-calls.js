589b8075ac08e6ebca8c9832f379fa50ac774e1f
bertdida <38489033+bertdida@users.noreply.github.com>
2020-07-22 21:23:29 +0800

    Added optional config param to fetchUser

--- src/common/UnpublishedQuotes/api-calls.js
+++ src/common/UnpublishedQuotes/api-calls.js
@@ -1,5 +1,5 @@
 import { axios } from 'common/axios';
 
-export function fetchUser(userId) {
-  return axios.get(`/v1/users/${userId}`);
+export function fetchUser(userId, config = {}) {
+  return axios.get(`/v1/users/${userId}`, config);
 }
