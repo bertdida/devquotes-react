aea4e41218ec345b3364ad51a0d4043cb1683577
bertdida <38489033+bertdida@users.noreply.github.com>
2020-07-23 21:35:30 +0800

    Set per_page query param to 25

--- src/pages/unpublished-quotes/api-calls.js
+++ src/pages/unpublished-quotes/api-calls.js
@@ -1,5 +1,5 @@
 import { axios } from 'common/axios';
 
 export function fetchQuotes(page) {
-  return axios.get(`/v1/quotes?is_published=false&page=${page}`);
+  return axios.get(`/v1/quotes?is_published=false&page=${page}&per_page=25`);
 }
