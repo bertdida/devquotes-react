7506c98fe0b26bdde237c9e1c0573f1b303bf70d
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-04 13:25:31 +0800

    Wrapped App inside ThemeProvider and SnackProvider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 
 import { App } from './App';
 import { ServiceWorkerProvider } from './common/hooks/useServiceWorker';
+import { SnackProvider } from './common/hooks/useSnack';
+import { ThemeProvider } from './common/hooks/useTheme';
 
 ReactDOM.render(
   <ServiceWorkerProvider>
-    <App />
+    <ThemeProvider>
+      <SnackProvider>
+        <App />
+      </SnackProvider>
+    </ThemeProvider>
   </ServiceWorkerProvider>,
   document.getElementById('root')
 );
