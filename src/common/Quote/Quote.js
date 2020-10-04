d7570860a4a984c5f7db3fdb2b109742da434a6c
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-04 11:59:07 +0800

    Added quote card actions

--- src/common/Quote/Quote.js
+++ src/common/Quote/Quote.js
@@ -3,13 +3,32 @@ import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkIcon from '@material-ui/icons/Link';
 
-import { Actions } from './Actions';
 import { useStyles } from './Quote.style';
 
-export function Quote({ quote, ...props }) {
+export function Quote({
+  quote,
+  toggleLike,
+  isLiking,
+  shareOnTwitter,
+  shareOnFacebook,
+  copyLink,
+}) {
   const classes = useStyles();
-  const { author, quotation } = quote;
+  const { author, quotation, is_liked, total_likes } = quote;
+
+  function _toggleLike() {
+    if (!isLiking) {
+      toggleLike();
+    }
+  }
 
   return (
     <Card component="blockquote" className={classes.quote}>
@@ -27,11 +46,48 @@ export function Quote({ quote, ...props }) {
         </Typography>
       </CardContent>
 
-      <Actions quote={quote} {...props} />
+      <CardActions>
+        <Button
+          disableElevation
+          color={is_liked ? 'secondary' : 'default'}
+          variant="contained"
+          className={`${classes.quote}__like`}
+          onClick={_toggleLike}
+          aria-label="like quote"
+          startIcon={<FavoriteIcon />}
+        >
+          {total_likes}
+        </Button>
+
+        <IconButton
+          className={`${classes.quote}__twitter`}
+          onClick={shareOnTwitter}
+          aria-label="share on twitter"
+        >
+          <TwitterIcon />
+        </IconButton>
+
+        <IconButton
+          className={`${classes.quote}__facebook`}
+          onClick={shareOnFacebook}
+          aria-label="share on facebook"
+        >
+          <FacebookIcon />
+        </IconButton>
+
+        <IconButton onClick={copyLink} aria-label="copy link">
+          <LinkIcon />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 }
 
 Quote.propTypes = {
   quote: PropTypes.object.isRequired,
+  toggleLike: PropTypes.func.isRequired,
+  isLiking: PropTypes.bool.isRequired,
+  shareOnTwitter: PropTypes.func.isRequired,
+  shareOnFacebook: PropTypes.func.isRequired,
+  copyLink: PropTypes.func.isRequired,
 };
--- src/common/Quote/Quote.style.js
+++ src/common/Quote/Quote.style.js
@@ -1,58 +1,72 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useStyles = makeStyles(theme => ({
-  skeleton: {
-    padding: theme.spacing(2),
-    margin: theme.spacing(2, 0),
-    minHeight: 215,
-    '&__author': {
-      height: theme.spacing(3),
-      width: '50%',
-      maxWidth: 300,
-      marginBottom: theme.spacing(1),
-    },
-    '&__quotation': {
-      height: 40,
-      maxWidth: 700,
-    },
-    '&__button': {
-      transform: 'none',
-      width: 70,
-      height: 35,
-      borderRadius: 40,
-      marginLeft: theme.spacing(1),
-    },
-  },
+export const useStyles = makeStyles(theme => {
+  const isDarkTheme = theme.palette.type === 'dark';
 
-  quote: {
-    padding: theme.spacing(2),
-    margin: theme.spacing(2, 0),
-    '&:hover': {
-      boxShadow: theme.shadows[8],
-    },
-    '&__inner': {
-      display: 'flex',
-      flexDirection: 'column-reverse',
-      marginBottom: theme.spacing(2),
-    },
-    '&__author': {
-      fontStyle: 'normal',
-      marginBottom: theme.spacing(2),
-    },
-  },
-
-  [theme.breakpoints.up('sm')]: {
+  return {
     skeleton: {
-      padding: theme.spacing(3),
-      margin: theme.spacing(3, 0),
-      '&__button': {
+      padding: theme.spacing(2),
+      margin: theme.spacing(2, 0),
+      minHeight: 215,
+      '&__author': {
+        height: theme.spacing(3),
+        width: '50%',
+        maxWidth: 300,
+        marginBottom: theme.spacing(1),
+      },
+      '&__quotation': {
         height: 40,
+        maxWidth: 700,
+      },
+      '&__button': {
+        transform: 'none',
+        width: 70,
+        height: 35,
+        borderRadius: 40,
+        marginLeft: theme.spacing(1),
       },
     },
 
     quote: {
-      padding: theme.spacing(3),
-      margin: theme.spacing(3, 0),
+      padding: theme.spacing(2),
+      margin: theme.spacing(2, 0),
+      '&:hover': {
+        boxShadow: theme.shadows[8],
+      },
+      '&__inner': {
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        marginBottom: theme.spacing(2),
+      },
+      '&__author': {
+        fontStyle: 'normal',
+        marginBottom: theme.spacing(2),
+      },
+      '&__like': {
+        marginRight: 'auto',
+        borderRadius: 25,
+      },
+      '&__twitter': {
+        color: isDarkTheme ? '#fff' : '#1da1f2',
+      },
+      '&__facebook': {
+        color: isDarkTheme ? '#fff' : '#298aff',
+      },
     },
-  },
-}));
+
+    [theme.breakpoints.up('sm')]: {
+      skeleton: {
+        padding: theme.spacing(3),
+        margin: theme.spacing(3, 0),
+        '&__button': {
+          height: 40,
+        },
+      },
+
+      quote: {
+        padding: theme.spacing(3),
+        margin: theme.spacing(3, 0),
+      },
+    },
+  };
+});
