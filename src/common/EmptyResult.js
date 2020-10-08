14d9d68516547b3ed8a9e5340fc147f1a138cbe4
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-08 21:55:38 +0800

    Added src/common/EmptyResult.js

---
+++ src/common/EmptyResult.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export function EmptyResult() {
+  return <Typography variant="h3">Nothing to show</Typography>;
+}
