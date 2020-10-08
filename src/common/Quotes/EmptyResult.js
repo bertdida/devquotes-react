1972164d7654f48a02df6e33dda6c4ee16244b2c
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-08 21:45:39 +0800

    Deleted  src/common/Quotes/EmptyResult.js

--- src/common/Quotes/EmptyResult.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-
-export function EmptyResult() {
-  return <Typography variant="h3">Nothing to show</Typography>;
-}
