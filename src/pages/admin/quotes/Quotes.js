2dd997d1cffed9a3379851451422c34d028f35cd
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-09 17:55:56 +0800

    Remove unecessary codes

--- src/pages/admin/quotes/Quotes.js
+++ src/pages/admin/quotes/Quotes.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
 import { Table } from './Table';
-import * as api from './api-calls';
 
-export function Quotes(props) {
-  function updatePage(page) {
-    props.history.push({ pathname: '/quotes', search: `?page=${page}` });
-  }
-
+export function Quotes() {
   return (
     <React.Fragment>
       <Helmet>
         <title>DevQuotes | Manage Quotes</title>
       </Helmet>
 
-      <Table fetchQuotes={api.fetchQuotes} updatePage={updatePage} {...props} />
+      <Table />
     </React.Fragment>
   );
 }
-
-Quotes.propTypes = {
-  history: PropTypes.object.isRequired,
-};
