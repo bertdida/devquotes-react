8aac4300782bb0ae19f6c4d2e788f884190b6618
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-11 16:57:51 +0800

    Conditionally show quote status select

--- src/components/QuoteForm/QuoteForm.js
+++ src/components/QuoteForm/QuoteForm.js
@@ -3,26 +3,49 @@ import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import ReCAPTCHA from 'react-google-recaptcha';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { isWebUri } from 'valid-url';
 
+import { useAuth } from 'common/hooks/useAuth';
 import { useStyles } from './QuoteForm.style';
+
+const STATUS_OPTIONS = [
+  {
+    text: 'Published',
+    value: 'published',
+  },
+  {
+    text: 'Pending Review',
+    value: 'pending_review',
+  },
+  {
+    text: 'Spam',
+    value: 'spam',
+  },
+];
 
 const QUOTATION_MAX_LENGTH = 200;
 const DEFAULT_FORM_VALUES = {
   author: '',
   quotation: '',
   source: null,
+  status: STATUS_OPTIONS[0].value,
 };
 
 export const QuoteForm = memo(WrappedQuoteForm);
 
 function WrappedQuoteForm({ quote: quoteProp, onSubmit }) {
+  const { user } = useAuth();
   const classes = useStyles();
 
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [quote, setQuote] = useState(DEFAULT_FORM_VALUES);
+  const isCreating = quote.id;
 
   useEffect(() => {
     setQuote(quoteProp || DEFAULT_FORM_VALUES);
@@ -97,6 +120,26 @@ function WrappedQuoteForm({ quote: quoteProp, onSubmit }) {
           helperText="The URL of the page with the quote"
           fullWidth
         />
+
+        {user.is_admin && isCreating && (
+          <FormControl margin="normal" fullWidth>
+            <InputLabel htmlFor="quote-status">Status</InputLabel>
+            <Select
+              value={quote.status}
+              onChange={handleChange}
+              inputProps={{
+                name: 'status',
+                id: 'quote-status',
+              }}
+            >
+              {STATUS_OPTIONS.map((option, index) => (
+                <MenuItem key={index} value={option.value}>
+                  {option.text}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        )}
 
         {process.env.REACT_APP_RECAPTCHA_KEY && (
           <ReCAPTCHA
