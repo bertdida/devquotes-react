e7b9f3429030ba279485cf0d7f38ab2bfc7c29f1
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-11 13:45:34 +0800

    Fix render

--- src/index.js
+++ src/index.js
@@ -2,17 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from './App';
-import { ServiceWorkerProvider } from './common/hooks/useServiceWorker';
-import { SnackProvider } from './common/hooks/useSnack';
-import { ThemeProvider } from './common/hooks/useTheme';
 
-ReactDOM.render(
-  <ServiceWorkerProvider>
-    <ThemeProvider>
-      <SnackProvider>
-        <App />
-      </SnackProvider>
-    </ThemeProvider>
-  </ServiceWorkerProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
