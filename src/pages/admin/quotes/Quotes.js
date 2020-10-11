14ec15c16e3d9ddb53f0a649892904bcd93d6b60
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-11 17:22:48 +0800

    Use src/pages/admin/quotes/Table/

--- src/pages/admin/quotes/Quotes.js
+++ src/pages/admin/quotes/Quotes.js
@@ -1,12 +1,7 @@
 import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import Paper from '@material-ui/core/Paper';
-import TableContainer from '@material-ui/core/TableContainer';
-import TablePagination from '@material-ui/core/TablePagination';
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import { useHistory } from 'react-router-dom';
-import queryString from 'query-string';
 
 import {
   QuotesProvider,
@@ -17,7 +12,6 @@ import {
 import { Toolbar } from './Toolbar';
 import { Table } from './Table';
 import { fetchQuotes, deleteQuotes } from './api-calls';
-import { useStyles } from './Quotes.style';
 
 const DEFAULT_PAGE = 1;
 const DEFAULT_PER_PAGE = 25;
@@ -35,24 +29,17 @@ export function Quotes() {
 }
 
 function WrappedQuotes() {
-  const state = useQuotesState();
-  const dispatch = useQuotesDispatch();
-  const classes = useStyles();
   const history = useHistory();
-
-  const { location } = history;
-  const { search } = location;
+  const dispatch = useQuotesDispatch();
+  const { quotes, queryParams } = useQuotesState();
 
   useEffect(() => {
+    const { search } = history.location;
     dispatch({ type: actions.PARSE_QUERY_PARAMS, payload: { params: search } });
-  }, [dispatch, search]);
+  }, [dispatch, history.location]);
 
   useEffect(() => {
-    // eslint-disable-next-line prefer-destructuring
-    const queryParams = state.queryParams;
-    if (queryParams === null) {
-      return;
-    }
+    if (queryParams === null) return;
 
     dispatch({ type: actions.QUOTES_LOADING });
     const { page = DEFAULT_PAGE, per_page = DEFAULT_PER_PAGE } = queryParams;
@@ -66,10 +53,10 @@ function WrappedQuotes() {
           return history.push('/404');
         }
       });
-  }, [dispatch, history, state.queryParams]);
+  }, [dispatch, history, queryParams]);
 
   async function deleteSelected() {
-    const ids = state.quotes.reduce((carry, quote) => {
+    const ids = quotes.reduce((carry, quote) => {
       if (!quote.isSelected) return carry;
       return [...carry, quote.id];
     }, []);
@@ -83,43 +70,13 @@ function WrappedQuotes() {
     dispatch({ type: actions.QUOTES_DELETED, payload: { ids: successIds } });
   }
 
-  function handleChangePage(_, newPage) {
-    const query = queryString.parse(location.search);
-    const newQuery = { ...query, page: newPage === 0 ? null : newPage + 1 };
-    const newQueryString = queryString.stringify(newQuery, { skipNull: true });
-
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    history.push({ pathname: '/admin/quotes', search: newQueryString });
-  }
-
-  const { quotes, pagination, isLoading } = state;
-  const numSelected = quotes.filter(({ isSelected }) => isSelected).length;
-
   return (
     <Paper>
       <Toolbar
-        totalSelectedQuotes={numSelected}
+        totalSelectedQuotes={quotes.filter(quote => quote.isSelected).length}
         deleteSelected={deleteSelected}
       />
-
-      <TableContainer className={classes.table}>
-        <Backdrop open={isLoading} className={`${classes.table}__backdrop`}>
-          <CircularProgress color="inherit" />
-        </Backdrop>
-
-        <Table />
-
-        {!isLoading && (
-          <TablePagination
-            component="div"
-            onChangePage={handleChangePage}
-            rowsPerPageOptions={[]}
-            count={pagination.total}
-            page={pagination.curr_page - 1} // zero based
-            rowsPerPage={pagination.per_page}
-          />
-        )}
-      </TableContainer>
+      <Table />
     </Paper>
   );
 }
