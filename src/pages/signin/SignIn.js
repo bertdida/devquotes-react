89232f312051350712fa3c66cfa330b92d005ade
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-11 11:52:29 +0800

    Update signin form styles

--- src/pages/signin/SignIn.js
+++ src/pages/signin/SignIn.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Redirect } from 'react-router-dom';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { Helmet } from 'react-helmet';
 import firebase from 'firebase/app';
 
 import { useAuth } from 'common/hooks/useAuth';
+import { useTheme } from 'common/hooks/useTheme';
 import './SignIn.scss';
 
 const uiConfig = {
@@ -22,18 +23,51 @@ const uiConfig = {
 
 export function SignIn() {
   const { user } = useAuth();
+  const { theme } = useTheme();
+  const root = useRef();
+
+  useEffect(() => {
+    const { palette } = theme;
+    const styleProperties = [
+      {
+        name: '--color-background',
+        value: palette.background.paper,
+      },
+      {
+        name: '--color-primary',
+        value: palette.secondary.main,
+      },
+      {
+        name: '--color-error',
+        value: palette.error.main,
+      },
+      {
+        name: '--color-button-text',
+        value: palette.primary.contrastText,
+      },
+    ];
+
+    styleProperties.forEach(({ name, value }) => {
+      root.current.style.setProperty(name, value);
+    });
+  }, [theme]);
 
   if (user) {
     return <Redirect to="/" />;
   }
 
   return (
-    <React.Fragment>
+    <>
       <Helmet>
         <title>DevQuotes | Sign In</title>
       </Helmet>
 
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-    </React.Fragment>
+      <div ref={root}>
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      </div>
+    </>
   );
 }
