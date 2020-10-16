8a78ccbb0069c51f14976f8fd5f3983b87f8def6
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-16 17:00:53 +0800

    Removed updatePage prop on CommonQuotes

--- src/pages/favorites/Favorites.js
+++ src/pages/favorites/Favorites.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { useHistory } from 'react-router-dom';
 
 import { Quotes } from 'components/Quotes';
 import api from 'common/api';
@@ -8,19 +7,13 @@ import api from 'common/api';
 const { fetchLikes } = api;
 
 export function Favorites() {
-  const history = useHistory();
-
-  function updatePage(page) {
-    history.push({ pathname: '/favorites', search: `?page=${page}` });
-  }
-
   return (
     <React.Fragment>
       <Helmet>
         <title>DevQuotes | Favorites</title>
       </Helmet>
 
-      <Quotes fetchQuotes={fetchLikes} updatePage={updatePage} />
+      <Quotes fetchQuotes={fetchLikes} />
     </React.Fragment>
   );
 }
--- src/pages/quotes/Quotes.js
+++ src/pages/quotes/Quotes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { useHistory } from 'react-router-dom';
 
 import { Quotes as CommonQuotes } from 'components/Quotes';
 import api from 'common/api';
@@ -8,19 +7,13 @@ import api from 'common/api';
 const { fetchQuotes } = api;
 
 export function Quotes() {
-  const history = useHistory();
-
-  function updatePage(page) {
-    history.push({ pathname: '/quotes', search: `?page=${page}` });
-  }
-
   return (
     <React.Fragment>
       <Helmet>
         <title>DevQuotes | Quotes</title>
       </Helmet>
 
-      <CommonQuotes fetchQuotes={fetchQuotes} updatePage={updatePage} />
+      <CommonQuotes fetchQuotes={fetchQuotes} />
     </React.Fragment>
   );
 }
