9200d35517ddab29fdc795464599a13d09df08ec
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-17 09:59:33 +0800

    Refactor: accept id only for likeQuote and unlikeQuote functions

--- src/common/api/likes.js
+++ src/common/api/likes.js
@@ -4,10 +4,10 @@ export function fetchLikes({ page }) {
   return axios.get(`/v1/likes?page=${page}`);
 }
 
-export function likeQuote(quote) {
-  return axios.post('/v1/likes', { ...quote });
+export function likeQuote(id) {
+  return axios.post('/v1/likes', { id });
 }
 
-export function unlikeQuote(quote) {
-  return axios.delete(`/v1/likes/${quote.id}`);
+export function unlikeQuote(id) {
+  return axios.delete(`/v1/likes/${id}`);
 }
