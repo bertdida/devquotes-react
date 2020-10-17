d9d3cdcdd5fc9af75594637055fddc0e6aca0ae9
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-17 10:04:59 +0800

    Refactor: accept id as first argument for fetchQuoteContributor

--- src/common/api/quotes.js
+++ src/common/api/quotes.js
@@ -30,8 +30,8 @@ export function deleteQuotes(ids) {
   return axios.delete(`/v1/quotes?ids=${ids.join(',')}`);
 }
 
-export function fetchQuoteContributor(quote, config = {}) {
-  return axios.get(`/v1/quotes/${quote.id}/contributor`, config);
+export function fetchQuoteContributor(id, config = {}) {
+  return axios.get(`/v1/quotes/${id}/contributor`, config);
 }
 
 export function searchQuote(query) {
