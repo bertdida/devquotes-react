644e67b73d285165efdbfa0d64db6fd620bd267a
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-18 17:50:09 +0800

    Fix Header component test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,45 +2,33 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import mediaQuery from 'css-mediaquery';
-import { createMuiTheme } from '@material-ui/core/styles';
 
 import { AuthContext } from 'common/hooks/useAuth';
 import { ThemeContext } from 'common/hooks/useTheme';
 import { Header } from './Header';
 
-const REGEX = {
+const RE_PATTERN = {
+  mainNav: /open main navigation/i,
   home: /home/i,
-  quotes: /quotes/i,
-  search: /search/i,
+  quotes: /^quotes/i,
   favorites: /favorites/i,
-  toggleTheme: /dark theme: on|off/i,
-  createQuote: /create quote/i,
   signIn: /sign in/i,
   signOut: /sign out/i,
-  moreMenu: /open more menu/i,
-  mainNav: /open main navigation/i,
+  toggleTheme: /dark theme: on|off/i,
+  createQuote: /create quote/i,
+  manageQuotes: /manage quotes/i,
+  submitQuote: /submit quote/i,
 };
-
-const { breakpoints } = createMuiTheme();
-
-function createMatchMedia(width) {
-  return query => ({
-    matches: mediaQuery.match(query, { width }),
-    addListener: () => {},
-    removeListener: () => {},
-  });
-}
 
 function renderHeader(user) {
   return render(
-    <ThemeContext.Provider value={{ isDarkMode: false, toggle: () => {} }}>
+    <BrowserRouter>
       <AuthContext.Provider value={{ user }}>
-        <BrowserRouter>
+        <ThemeContext.Provider value={{ isDarkMode: false, toggle: () => {} }}>
           <Header />
-        </BrowserRouter>
+        </ThemeContext.Provider>
       </AuthContext.Provider>
-    </ThemeContext.Provider>
+    </BrowserRouter>
   );
 }
 
@@ -48,110 +36,53 @@ function expectNull(node) {
   expect(node).toBeNull();
 }
 
-describe('Medium to large screen', () => {
-  beforeAll(() => {
-    window.matchMedia = createMatchMedia(breakpoints.values.md);
-  });
+it('renders nav items for anonymous user', () => {
+  const { getByRole, queryByRole } = renderHeader(null);
 
-  it('renders for anonymous user', () => {
-    const { getByRole, queryByRole } = renderHeader(null);
+  fireEvent.click(getByRole('button', { name: RE_PATTERN.mainNav }));
 
-    getByRole('button', { name: REGEX.home });
-    getByRole('button', { name: REGEX.quotes });
-    getByRole('button', { name: REGEX.search });
+  getByRole('button', { name: RE_PATTERN.home });
+  getByRole('button', { name: RE_PATTERN.quotes });
+  getByRole('button', { name: RE_PATTERN.favorites });
+  getByRole('button', { name: RE_PATTERN.signIn });
+  getByRole('button', { name: RE_PATTERN.toggleTheme });
+  getByRole('button', { name: RE_PATTERN.submitQuote });
 
-    getByRole('button', { name: REGEX.signIn });
-    expectNull(queryByRole('button', { name: REGEX.favorites }));
-
-    fireEvent.click(getByRole('button', { name: REGEX.moreMenu }));
-    getByRole('menuitem', { name: REGEX.toggleTheme });
-    expectNull(queryByRole('menuitem', { name: REGEX.createQuote }));
-    expectNull(queryByRole('menuitem', { name: REGEX.signOut }));
-  });
-
-  it('renders for authenticated user', () => {
-    const { getByRole, queryByRole } = renderHeader({ is_admin: false });
-
-    getByRole('button', { name: REGEX.home });
-    getByRole('button', { name: REGEX.quotes });
-    getByRole('button', { name: REGEX.search });
-
-    expectNull(queryByRole('button', { name: REGEX.signIn }));
-    getByRole('button', { name: REGEX.favorites });
-
-    fireEvent.click(getByRole('button', { name: REGEX.moreMenu }));
-    getByRole('menuitem', { name: REGEX.toggleTheme });
-    expectNull(queryByRole('menuitem', { name: REGEX.createQuote }));
-    getByRole('menuitem', { name: REGEX.signOut });
-  });
-
-  it('renders for admin user', () => {
-    const { getByRole, queryByRole } = renderHeader({ is_admin: true });
-
-    getByRole('button', { name: REGEX.home });
-    getByRole('button', { name: REGEX.quotes });
-    getByRole('button', { name: REGEX.search });
-
-    expectNull(queryByRole('button', { name: REGEX.signIn }));
-    getByRole('button', { name: REGEX.favorites });
-
-    fireEvent.click(getByRole('button', { name: REGEX.moreMenu }));
-    getByRole('menuitem', { name: REGEX.toggleTheme });
-    getByRole('menuitem', { name: REGEX.createQuote });
-    getByRole('menuitem', { name: REGEX.signOut });
-  });
+  expectNull(queryByRole('button', { name: RE_PATTERN.createQuote }));
+  expectNull(queryByRole('button', { name: RE_PATTERN.manageQuotes }));
+  expectNull(queryByRole('button', { name: RE_PATTERN.signOut }));
 });
 
-describe('Small to  medium screen', () => {
-  beforeAll(() => {
-    window.matchMedia = createMatchMedia(breakpoints.values.sm);
-  });
+it('renders nav items for authenticated user', () => {
+  const { getByRole, queryByRole } = renderHeader({ is_admin: false });
 
-  it('renders for anonymous user', () => {
-    const { getByRole, queryByRole } = renderHeader(null);
+  fireEvent.click(getByRole('button', { name: RE_PATTERN.mainNav }));
 
-    fireEvent.click(getByRole('button', { name: REGEX.mainNav }));
-    getByRole('button', { name: REGEX.home });
-    getByRole('button', { name: REGEX.quotes });
-    getByRole('button', { name: REGEX.search });
-    getByRole('button', { name: REGEX.toggleTheme });
+  getByRole('button', { name: RE_PATTERN.home });
+  getByRole('button', { name: RE_PATTERN.quotes });
+  getByRole('button', { name: RE_PATTERN.favorites });
+  getByRole('button', { name: RE_PATTERN.signOut });
+  getByRole('button', { name: RE_PATTERN.toggleTheme });
+  getByRole('button', { name: RE_PATTERN.submitQuote });
 
-    getByRole('button', { name: REGEX.signIn });
+  expectNull(queryByRole('button', { name: RE_PATTERN.createQuote }));
+  expectNull(queryByRole('button', { name: RE_PATTERN.manageQuotes }));
+  expectNull(queryByRole('button', { name: RE_PATTERN.signIn }));
+});
 
-    expectNull(queryByRole('button', { name: REGEX.favorites }));
-    expectNull(queryByRole('button', { name: REGEX.createQuote }));
-    expectNull(queryByRole('button', { name: REGEX.signOut }));
-  });
+it('renders nav items for admin user', () => {
+  const { getByRole, queryByRole } = renderHeader({ is_admin: true });
 
-  it('renders for authenticated user', () => {
-    const { getByRole, queryByRole } = renderHeader({ is_admin: false });
+  fireEvent.click(getByRole('button', { name: RE_PATTERN.mainNav }));
 
-    fireEvent.click(getByRole('button', { name: REGEX.mainNav }));
-    getByRole('button', { name: REGEX.home });
-    getByRole('button', { name: REGEX.quotes });
-    getByRole('button', { name: REGEX.search });
-    getByRole('button', { name: REGEX.toggleTheme });
+  getByRole('button', { name: RE_PATTERN.home });
+  getByRole('button', { name: RE_PATTERN.quotes });
+  getByRole('button', { name: RE_PATTERN.favorites });
+  getByRole('button', { name: RE_PATTERN.signOut });
+  getByRole('button', { name: RE_PATTERN.toggleTheme });
+  getByRole('button', { name: RE_PATTERN.createQuote });
+  getByRole('button', { name: RE_PATTERN.manageQuotes });
 
-    expectNull(queryByRole('button', { name: REGEX.signIn }));
-
-    getByRole('button', { name: REGEX.favorites });
-    expectNull(queryByRole('button', { name: REGEX.createQuote }));
-    getByRole('button', { name: REGEX.signOut });
-  });
-
-  it('renders for admin user', () => {
-    const { getByRole, queryByRole } = renderHeader({ is_admin: true });
-
-    fireEvent.click(getByRole('button', { name: REGEX.mainNav }));
-    getByRole('button', { name: REGEX.home });
-    getByRole('button', { name: REGEX.quotes });
-    getByRole('button', { name: REGEX.search });
-    getByRole('button', { name: REGEX.toggleTheme });
-
-    expectNull(queryByRole('button', { name: REGEX.signIn }));
-
-    getByRole('button', { name: REGEX.favorites });
-    getByRole('button', { name: REGEX.createQuote });
-    getByRole('button', { name: REGEX.signOut });
-  });
+  expectNull(queryByRole('button', { name: RE_PATTERN.submitQuote }));
+  expectNull(queryByRole('button', { name: RE_PATTERN.signIn }));
 });
