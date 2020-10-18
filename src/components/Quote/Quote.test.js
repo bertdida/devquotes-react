3e37f6551cf7c87cb8398000476970fbb6eba746
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-18 15:14:37 +0800

    Fixed Quote component test

--- src/components/Quote/Quote.test.js
+++ src/components/Quote/Quote.test.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import { render, fireEvent, waitFor, within } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import axiosMock from 'axios';
 
 import { AuthContext } from 'common/hooks/useAuth';
+import { SnackContext } from 'common/hooks/useSnack';
 import { Quote } from './Quote';
-
-jest.mock('axios');
-
-const REGEX = {
-  like: /like quote/i,
-  shareTwitter: /share on twitter/i,
-  shareFacebook: /share on facebook/i,
-  copyLink: /copy link/i,
-  edit: /edit quote/i,
-  moreMenu: /open more menu/i,
-  delete: /open delete confirmation dialog/i,
-  deleteTitle: /delete quote?/i,
-  deleteConfirm: /yes, delete quote/i,
-};
 
 const quote = {
   id: 1,
@@ -26,15 +12,13 @@ const quote = {
   quotation: 'Quotation 1',
 };
 
-function expectNull(node) {
-  expect(node).toBeNull();
-}
-
-it('renders semantic html', () => {
-  const { container } = render(
-    <AuthContext.Provider value={{ user: null }}>
-      <Quote quote={quote} />
-    </AuthContext.Provider>
+it('renders quote', () => {
+  const { getByRole, container } = render(
+    <SnackContext.Provider value={{ dispatch: () => {} }}>
+      <AuthContext.Provider value={{ user: null }}>
+        <Quote quote={quote} />
+      </AuthContext.Provider>
+    </SnackContext.Provider>
   );
 
   const root = container.querySelector('blockquote');
@@ -43,62 +27,9 @@ it('renders semantic html', () => {
 
   expect(author.nodeName).toBe('CITE');
   expect(quotation.nodeName).toBe('P');
-});
 
-it('renders actions for user', () => {
-  const { getByRole, queryByRole } = render(
-    <AuthContext.Provider value={{ user: null }}>
-      <Quote quote={quote} />
-    </AuthContext.Provider>
-  );
-
-  expectNull(queryByRole('button', { name: REGEX.moreMenu }));
-  expectNull(queryByRole('menuitem', { name: REGEX.edit }));
-  expectNull(queryByRole('menuitem', { name: REGEX.delete }));
-  expectNull(queryByRole('menuitem', { name: REGEX.copyLink }));
-
-  getByRole('button', { name: REGEX.like });
-  getByRole('button', { name: REGEX.shareTwitter });
-  getByRole('button', { name: REGEX.shareFacebook });
-  getByRole('button', { name: REGEX.copyLink });
-});
-
-it('renders actions for admin', () => {
-  const { getByRole, queryByRole } = render(
-    <AuthContext.Provider value={{ user: { is_admin: true } }}>
-      <Quote quote={quote} />
-    </AuthContext.Provider>
-  );
-
-  expectNull(queryByRole('button', { name: REGEX.shareTwitter }));
-  expectNull(queryByRole('button', { name: REGEX.shareFacebook }));
-  expectNull(queryByRole('button', { name: REGEX.copyLink }));
-
-  getByRole('button', { name: REGEX.like });
-  getByRole('button', { name: REGEX.edit });
-  getByRole('button', { name: REGEX.delete });
-
-  fireEvent.click(getByRole('button', { name: REGEX.moreMenu }));
-  getByRole('menuitem', { name: REGEX.shareTwitter });
-  getByRole('menuitem', { name: REGEX.shareFacebook });
-  getByRole('menuitem', { name: REGEX.copyLink });
-});
-
-it('renders no actions when deleted', async () => {
-  axiosMock.delete.mockResolvedValueOnce({ data: null });
-
-  const { getByRole, getByText } = render(
-    <AuthContext.Provider value={{ user: { is_admin: true } }}>
-      <Quote quote={quote} />
-    </AuthContext.Provider>
-  );
-
-  fireEvent.click(getByRole('button', { name: REGEX.delete }));
-  getByRole('dialog', { heading: REGEX.deleteTitle });
-
-  fireEvent.click(getByRole('button', { name: REGEX.deleteConfirm }));
-  await waitFor(() => getByText('This quote has been deleted.'));
-
-  expect(axiosMock.delete).toHaveBeenCalledTimes(1);
-  expect(axiosMock.delete).toHaveBeenCalledWith(`/v1/quotes/${quote.id}`);
+  getByRole('button', { name: /like quote/i });
+  getByRole('button', { name: /share on twitter/i });
+  getByRole('button', { name: /share on facebook/i });
+  getByRole('button', { name: /copy link/i });
 });
