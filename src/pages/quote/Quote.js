b50991cab9f33429a4c7ec6420d3acb6e117cd9c
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-19 17:41:14 +0800

    Fix: Quote component rerenders when switching theme mode

--- src/pages/quote/Quote.js
+++ src/pages/quote/Quote.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, memo } from 'react';
+import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { Redirect, useHistory, useRouteMatch } from 'react-router-dom';
 
@@ -9,9 +10,23 @@ import api from 'common/api';
 const { fetchQuote } = api;
 
 export function Quote() {
-  const history = useHistory();
-  const match = useRouteMatch();
+  // useRouterMatch returns a new object on each render — which causes
+  // rerenders and extra API calls in our case. We can avoid that by
+  // memoizing Quote and implement our custom equality check for params.
+  // GH issue: https://github.com/ReactTraining/react-router/issues/7059
 
+  const { params } = useRouteMatch();
+  return <MemoizedQuote params={params} />;
+}
+
+const MemoizedQuote = memo(WrappedQuote, (prevProps, nextProps) => {
+  const { params: prevParams } = prevProps;
+  const { params: nextParams } = nextProps;
+  return prevParams.id === nextParams.id && prevParams.slug === nextParams.slug;
+});
+
+function WrappedQuote({ params: paramsProp }) {
+  const history = useHistory();
   const [params, setParams] = useState(null);
   const [quote, setQuote] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -38,8 +53,8 @@ export function Quote() {
 
   useEffect(() => {
     setQuote(null);
-    setParams(match.params);
-  }, [match.params]);
+    setParams(paramsProp);
+  }, [paramsProp]);
 
   if (isLoading || quote === null) {
     return <QuoteSkeleton />;
@@ -59,3 +74,7 @@ export function Quote() {
     </>
   );
 }
+
+WrappedQuote.propTypes = {
+  params: PropTypes.object.isRequired,
+};
