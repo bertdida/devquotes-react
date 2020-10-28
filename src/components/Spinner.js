dfa0c19972e0836ad4cc0248365fbd456ddde9fe
bertdida <38489033+bertdida@users.noreply.github.com>
2020-10-28 13:24:22 +0800

    Added src/components/Spinner.js

---
+++ src/components/Spinner.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  message: {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+export function Spinner({ message = null }) {
+  const classes = useStyles();
+
+  return (
+    <Box display="flex" alignItems="center" justifyContent="center">
+      <CircularProgress size={20} />
+      {message && (
+        <Typography className={classes.message}>{message}</Typography>
+      )}
+    </Box>
+  );
+}
+
+Spinner.propTypes = {
+  message: PropTypes.string,
+};
